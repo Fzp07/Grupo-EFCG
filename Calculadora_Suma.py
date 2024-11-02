3a64a86fa8939e879640365a4392fd401db49e09
Candresh9 <Candresh9@gmail.com>
2024-11-02 11:12:33 -0500

    Create Calculadora Suma

---
+++ Calculadora_Suma.py
@@ -0,0 +1,9 @@
+def sum(a, b):
+    return a + b
+
+
+a = int(input('Introduce el primer número: '))
+
+b = int(input('Introduce el segundo número: '))
+
+print(f'Suma de {a} y {b} es: {sum(a, b)}')
