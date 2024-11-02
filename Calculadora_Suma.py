1052eb63678329e68b46409927ce31521ce639c0
Fares Zuñiga Perez <faresnezu15@gmail.com>
2024-11-02 11:30:11 -0500

    Delete Calculadora_Suma.py

--- Calculadora_Suma.py
+++
@@ -1,9 +0,0 @@
-def sum(a, b):
-    return a + b
-
-
-a = int(input('Introduce el primer número: '))
-
-b = int(input('Introduce el segundo número: '))
-
-print(f'Suma de {a} y {b} es: {sum(a, b)}')
